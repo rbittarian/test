ca64d549b8f8928b3730db56c67cf9e6a23299c9
rbittarian <rbittarian@gmail.com>
2011-01-26 00:40:44 -0600

    Added a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
-module ApplicationHelper
+module ApplicationHelper 
+  
+  # Return a title on a per-page basis.
+  def title
+      base_title = "ROR sample app" 
+      return base_title if @title.nil?
+      base_title + " | #{@title}"  
+  end
 end
